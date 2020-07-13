26ceaaeed85bbc5643529e213510434a2f7eed20
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-13 18:40:15 +0530

    Jul 13  18:40:14

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -20,7 +20,7 @@ def fun_matrixmultiply(m1, m2):
         for i in range(len(m1)):
             for j in range(len(m2[0])):
                 for k in range(len(m2)):
-                    c[i][j] += m1[i][k] * m2[k][j]
+                    c[i][j] += (m1[i][k] * m2[k][j])
                     # print(c[i][j])
         return c
 
