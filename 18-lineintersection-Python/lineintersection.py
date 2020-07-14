d60c6fd0b931192c567982c789f412ca19c35ec1
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-14 18:49:17 +0530

    Jul 14  18:49:16

--- 18-lineintersection-Python/lineintersection.py
+++ 18-lineintersection-Python/lineintersection.py
@@ -6,4 +6,10 @@
 
 def lineintersection(m1, b1, m2, b2):
 	# your code goes here
-	pass
+	if(m1 == m2 or m1%m2 == 0 or m2%m1 == 0):
+		return None
+	else:
+		x = round((b2 - b1)/(m1 - m2))
+		return x
+
+print(lineintersection(4, 13, 8, 17))
