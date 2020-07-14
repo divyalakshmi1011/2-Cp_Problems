e44fd8386113ca01609ac14353e5223f8d7f4f58
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-14 12:05:01 +0530

    Jul 14  12:05:01

--- 07-isfactor-Python/isfactor.py
+++ 07-isfactor-Python/isfactor.py
@@ -7,13 +7,14 @@
 
 def fun_isfactor(f, n):
 	if(n == 0):
-		return False
-	if(f == 0):
 		if(f == 0):
-			return True
-		else:
 			return False
 
+		else:
+			return True
+	if(f == 0):
+		return False
+		
 	if(n%f == 0):
 		return True
 	return False # replace with your solution
