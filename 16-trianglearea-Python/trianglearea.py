294518b452a953639a55fbb4133ca36948c08947
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-14 17:37:50 +0530

    tri

--- 16-trianglearea-Python/trianglearea.py
+++ 16-trianglearea-Python/trianglearea.py
@@ -2,8 +2,20 @@
 # Write the function trianglearea(s1, s2, s3) that takes 3 floats/ints and returns the area of the
 # triangle that has those lengths of its side. If no such triangle exists, return 0. Hint: you
 # will probably wish to use Heron's Formula.
+import math
 
+def islegaltriangle(s1, s2, s3):
+	if (s1 + s2 <= s3) or (s1 + s3 <= s2) or (s2 + s3 <= s1) :
+		return False
+	else:
+		return True
 
 def trianglearea(s1, s2, s3):
 	# your code goes here
-	pass
+	s = (s1 + s2 + s3) / 2
+	if(islegaltriangle(s1,s2,s3)):
+		temp = (s)*(s - s1)*(s - s2)*(s - s3)
+		area = math.sqrt(temp)
+		return area
+	else:
+		return 0
