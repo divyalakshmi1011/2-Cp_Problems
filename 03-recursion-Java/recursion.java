1769c7edff6f05ec25f0af9b04527627f12dbb85
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-15 11:38:07 +0530

    Jul 15  11:38:07

--- 03-recursion-Java/recursion.java
+++ 03-recursion-Java/recursion.java
@@ -1,5 +1,16 @@
+/**Implement a function recursively to get the desired
+Fibonacci sequence value.
+Your code should have the same input/output as the 
+iterative code in the instructions.**/
+
 class recursion {
-	public int get_fib(int value){
-	    return -1; 
+	public static int get_fib(int n){
+		if(n <= 1) {
+			return n;
+		}
+		return get_fib(n - 1) + get_fib(n - 2);
+	}
+	public static void main(String[] args) {
+		System.out.println(get_fib(10));
 	}
 }
