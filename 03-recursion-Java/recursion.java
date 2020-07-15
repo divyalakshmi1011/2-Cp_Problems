1476bc1c7e0d959bacc943d1469891164a00e9f4
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-15 11:39:43 +0530

    recursion

--- 03-recursion-Java/recursion.java
+++ 03-recursion-Java/recursion.java
@@ -11,6 +11,6 @@ class recursion {
 		return get_fib(n - 1) + get_fib(n - 2);
 	}
 	public static void main(String[] args) {
-		System.out.println(get_fib(10));
+		System.out.println(get_fib(9));
 	}
 }
