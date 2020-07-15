e4d553ec09b3cfa51b5992ab576ffb2a3a629812
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-15 12:34:50 +0530

    Jul 15  12:34:50

--- 05-pascaltrianglevalue-Java/pascaltrianglevalue.java
+++ 05-pascaltrianglevalue-Java/pascaltrianglevalue.java
@@ -3,14 +3,16 @@
 // # value in the given row and column of Pascal's 
 // # Triangle where the triangle starts at row 0, and 
 // # each row starts at column 0. If either row or col 
-// # are not legal values, return None, instead of crashing. 
+// # are not legal values, return None, instead of crashing.
 
 class pascaltrianglevalue {
-	public int fun_pascaltrianglevalue(int row, int col){
-		if(row == 0 || col == 0 || row == col) {
+	public int fun_pascaltrianglevalue(int i, int j){
+		if (j == 0) {
+			return 1;
+		} else if (j == i) {
 			return 1;
 		} else {
-			return fun_pascaltrianglevalue(row - 1, col - 1) + fun_pascaltrianglevalue(row - 1, col);
+			return fun_pascaltrianglevalue(i - 1, j - 1) + fun_pascaltrianglevalue(i - 1, j);
 		}
 	}
 }
