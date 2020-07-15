8534577211791f5d269d124d462bbe952e1bfc4e
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-15 15:06:39 +0530

    Jul 15  15:06:38

--- 06-get_kth_digit-Java/get_kth_digit.java
+++ 06-get_kth_digit-Java/get_kth_digit.java
@@ -8,7 +8,18 @@
 class get_kth_digit {
 	public int fun_get_kth_digit(int digit, int k){
 		// your code goes here
-		
+		String s = digit + "";
+		if(k < s.length()) {
+			StringBuilder input1 = new StringBuilder(); 
+  
+			// append a string into StringBuilder input1 
+			input1.append(s); 
+	
+			// reverse StringBuilder input1 
+			input1 = input1.reverse();
+			return Integer.parseInt((input1.toString().charAt(k)) + "");
+    
+		}
 		return 0;
 	}
 }
