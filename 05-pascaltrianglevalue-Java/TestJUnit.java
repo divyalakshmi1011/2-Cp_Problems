3bd99e45c4d2f04ba5d40a7013d3d997c380185e
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-15 12:40:39 +0530

    Jul 15  12:40:39

--- 05-pascaltrianglevalue-Java/TestJUnit.java
+++ 05-pascaltrianglevalue-Java/TestJUnit.java
@@ -18,6 +18,7 @@ public class TestJUnit {
 
    @Test
    public void testCase1() {
+      System.out.println("divya");
       pascaltrianglevalue s = new pascaltrianglevalue();
       assertEquals("1.", 1, s.fun_pascaltrianglevalue(1,1));
       assertEquals("2.", 0, s.fun_pascaltrianglevalue(3,5));
@@ -25,13 +26,13 @@ public class TestJUnit {
       assertEquals("4.", 10, s.fun_pascaltrianglevalue(5,2));
     }
 
-   // @Test
-   // public void testCase2() {
-   //    pascaltrianglevalue s = new pascaltrianglevalue();
-   //    assertEquals("1.", 20, s.fun_pascaltrianglevalue(6, 3));
-   //    assertEquals("2.", 15, s.fun_pascaltrianglevalue(6, 2));
-   //    assertEquals("3.", 35, s.fun_pascaltrianglevalue(7, 4));
-   //    assertEquals("4.", 0, s.fun_pascaltrianglevalue(7, 9));  
-   // }
+   @Test
+   public void testCase2() {
+      pascaltrianglevalue s = new pascaltrianglevalue();
+      assertEquals("1.", 20, s.fun_pascaltrianglevalue(6, 3));
+      assertEquals("2.", 15, s.fun_pascaltrianglevalue(6, 2));
+      assertEquals("3.", 35, s.fun_pascaltrianglevalue(7, 4));
+      assertEquals("4.", 0, s.fun_pascaltrianglevalue(7, 9));  
+   }
 }
 
