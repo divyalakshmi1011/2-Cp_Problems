6ed44645d4db1cd86563c6261f21bbe856ce8946
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-16 16:12:50 +0530

    Jul 16  16:12:50

--- 08-isrotated-Python/isrotated.py
+++ 08-isrotated-Python/isrotated.py
@@ -2,19 +2,21 @@
 # Write an efficient program to test if two given String is a rotation of each other or not, e.g. 
 # if given String is "XYZ" and "ZXY" then your function should return true, but if the input is 
 # "XYZ" and "YXZ" then return false.
+def rotate(input,d): 
+    # slice string in two parts for left and right 
+    Lfirst = input[0 : d] 
+    Lsecond = input[d :] 
+    Rfirst = input[0 : len(input)-d] 
+    Rsecond = input[len(input)-d : ] 
+    return (Lfirst+Lsecond)
+
 
 
 def isrotated(str1, str2):
-	l = len(str1)
-	r = str1[::-1]
-	for i in range(l):
-		a = r[i]
-		b = str1[:i]
-		c = str1[i + 1:]
-		print(a,b,c)
-		if(a+c+b == str2):
+	for i in range(len(str1)):
+		d = rotate(str1,i)
+		if(d == str2):
 			return True
 	return False
-
 
 print(isrotated("XYZ","ZXY"))
