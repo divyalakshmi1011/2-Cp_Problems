50f70d707d786c582bf197bbb7782103a89ad94d
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-16 15:43:59 +0530

    Jul 16  15:43:59

--- 07-removeduplicate-Python/removeduplicate.py
+++ 07-removeduplicate-Python/removeduplicate.py
@@ -3,10 +3,24 @@
 # if given String is "JavaPython" then the output should be "JavPython".
 # The second or further occurrence of duplicate should be removed.
 
+
 def removeduplicate(text):
 	# Your code goes here
-	l = list(text)
-	s = set(l)
-	print(l)
-
+	str = list(text)
+	n = len(text)
+	# Used as index in the modified string 
+	index = 0
+    # Traverse through all characters 
+	for i in range(0, n):  
+        # Check if str[i] is present before it  
+		for j in range(0, i + 1): 
+			if (str[i] == str[j]): 
+				break
+        # If not present, then add it to 
+        # result. 
+		if (j == i): 
+			str[index] = str[i] 
+			index += 1
+	return "".join(str[:index])
+	
 removeduplicate("javapython")
