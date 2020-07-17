84b690ad9d6a6c6e6b3d8d261f369422c1783ab9
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-17 15:26:07 +0530

    Jul 17  15:26:06

--- 05-binarytreepractice-Java/BinaryTree.java
+++ 05-binarytreepractice-Java/BinaryTree.java
@@ -13,17 +13,37 @@ public class BinaryTree {
 	public Node root;
 	
 	public BinaryTree(int value) {
-        // Your code goes here
+		this.root.value = value;
 	}
 
 	public boolean search(int value) {
-		// Your code goes here
-		return true;
+		return search_Node(this.root, value);
 	}
 
-	private boolean search_Node(Node node, int value) {
-		// Your code goes here
-		return false;
-
+	private boolean search_Node(Node r, int val) {
+		boolean found = false;
+         while ((r != null) && !found)
+         {
+             int rval = r.value;
+             if (val < rval)
+                 r = r.left;
+             else if (val > rval)
+                 r = r.right;
+             else
+             {
+                 found = true;
+                 break;
+             }
+             found = search_Node(r, val);
+         }
+         return found;
+	}
+	public static void main(String[] args){
+		BinaryTree s = new BinaryTree(1);
+		s.root.left = new Node(2);
+		s.root.right = new Node(3);
+		s.root.left.left = new Node(4);
+		s.root.left.right = new Node(5);
+		System.out.println(s.search(4));
 	}
 }
