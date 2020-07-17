5c22658f1f46b9f7f4f0c32e5ad34981685db7d2
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-17 16:11:30 +0530

    Jul 17  16:11:29

--- 05-binarytreepractice-Java/BinaryTree.java
+++ 05-binarytreepractice-Java/BinaryTree.java
@@ -18,36 +18,41 @@ public class BinaryTree {
 	}
 
 	public boolean search(int value) {
-		return search_Node(this.root, value);
+		flag = false;
+		searchNode(this.root, value);
+		return flag;
 	}
 
-	private boolean search_Node(Node temp, int value) {
-		if(root == null){  
-		     flag = false;
-		  }  
-		  else{  
-			//If value is found in the given binary tree then, set the flag to true  
-			if(temp.value == value){  
-			  flag = true;   
-			}  
-			//Search in left subtree  
-			if(flag == false && temp.left != null){  
-			   search_Node(temp.left, value);  
-			}  
-			//Search in right subtree  
-			if(flag == false && temp.right != null){  
-			   search_Node(temp.right, value);  
-			}  
-
-		  }
-		  return flag;  
-	}
+	public void searchNode(Node temp, int value){  
+        //Check whether tree is empty  
+        if(root == null){  
+          System.out.println("Tree is empty");  
+        }  
+        else{  
+          //If value is found in the given binary tree then, set the flag to true  
+          if(temp.value == value){  
+            flag = true;  
+               return;  
+          }  
+          //Search in left subtree  
+          if(flag == false && temp.left != null){  
+             searchNode(temp.left, value);  
+          }  
+          //Search in right subtree  
+          if(flag == false && temp.right != null){  
+             searchNode(temp.right, value);  
+          }  
+        }  
+      }  
 	public static void main(String[] args){
 		BinaryTree s = new BinaryTree(1);
 		s.root.left = new Node(2);
 		s.root.right = new Node(3);
 		s.root.left.left = new Node(4);
 		s.root.left.right = new Node(5);
-		System.out.println(s.search(2));
+		// s.searchNode(s.root,6);
+		// System.out.println(flag);
+		System.out.println(s.search(4));
+		System.out.println(s.search(6));
 	}
 }
