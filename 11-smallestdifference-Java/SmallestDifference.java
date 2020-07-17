2a7b3e830d4be0edc379734fda280d4b6865fbff
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-17 09:33:38 +0530

    Jul 17  09:33:37

--- 11-smallestdifference-Java/SmallestDifference.java
+++ 11-smallestdifference-Java/SmallestDifference.java
@@ -4,19 +4,21 @@
 //       assert(smallestDifference([19,2,83,6,27]) == 4)
 // The two closest numbers in that list are 2 and 6, and their difference is 4.
 import java.util.*;
-
+import java.lang.*;
+import java.lang.Math; 
 public class SmallestDifference {
 
     public int smallestDifference(int[] a) {
         int n = a.length;
-        int sum = 10^2;
-        for(int i = 0; i < n -1;i++){
-            for(int j = 0; j < n;j++){
-                if(sum < (a[i] - a[j])) {
-                    sum = (a[i] - a[j]);
-                }
-            }
-        }
-        return sum;
+        Arrays.sort(a);
+           // Initialize difference as infinite 
+           int diff = Integer.MAX_VALUE; 
+           // Find the min diff by comparing adjacent 
+           // pairs in sorted array 
+           for (int i=0; i<n-1; i++) 
+              if (a[i+1] - a[i] < diff) 
+                  diff = a[i+1] - a[i];
+           // Return min diff 
+           return diff; 
     }
 }
