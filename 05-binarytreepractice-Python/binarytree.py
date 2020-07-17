80ce9b8ba1641bb0dcdf14d692cf8420616ff560
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-17 15:48:49 +0530

    bst

---
+++ 05-binarytreepractice-Python/binarytree.py
@@ -0,0 +1,40 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def search(self, find_val):
+        if find_val < self.root.value:
+            if self.left is None:
+                return False
+            return self.left.serach(find_val)
+        elif find_val > self.root.value:
+            if self.right is None:
+                return False
+            return self.right.findval(find_val)
+        else:
+            return self.root.value
+
+    def print_tree(self):
+        """Print out all tree nodes
+        as they are visited in
+        a pre-order traversal."""
+        # Your code goes here
+        pass
+
+    def preorder_search(self, start, find_val):
+        """Helper method - use this to create a 
+        recursive search solution."""
+        # Your code goes here
+        pass
+
+    def preorder_print(self, start, traversal):
+        """Helper method - use this to create a 
+        recursive print solution."""
+        # Your code goes here
+        pass
