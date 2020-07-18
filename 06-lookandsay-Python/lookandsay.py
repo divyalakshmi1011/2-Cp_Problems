f5ae51261c6f422c5e37a7f5b17e356738c24aa6
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-18 14:05:29 +0530

    Jul 18  14:05:29

--- 06-lookandsay-Python/lookandsay.py
+++ 06-lookandsay-Python/lookandsay.py
@@ -1,4 +1,4 @@
-# First, you can read about look-and-say numbers here. Then, write the function lookAndSay(a) that takes a list of 
+# First, you can read about look-and-say as here. Then, write the function lookAndSay(a) that takes a list of 
 # numbers and returns a list of numbers that results from "reading off" the initial list using the look-and-say 
 # method, using tuples for each (count, value) pair. For example:
 # lookAndSay([]) == []
@@ -8,5 +8,15 @@
 # lookAndSay([3,3,8,3,3,3,3]) == [(2,3),(1,8),(4,3)]
 
 def lookandsay(a):
-	# Your code goes here
-	pass
+    result = []
+    repeat = a[0]
+    a = a[1:]+" "
+    count = 1
+    for item in a:
+        if item != repeat:
+            result.append(str(times),repeat)
+            count = 1
+            repeat = item
+        else:
+            count += 1
+    return result
