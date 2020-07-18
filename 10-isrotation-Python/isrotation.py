86d734160cdcadf91899b094fa6c0ca7ef59df17
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-18 10:16:46 +0530

    Jul 18  10:16:46

--- 10-isrotation-Python/isrotation.py
+++ 10-isrotation-Python/isrotation.py
@@ -4,7 +4,22 @@
 # returns True if x is a rotation of the digits of y and False otherwise. For example, 
 # 3412 is a rotation of 1234. Any number 
 # is a rotation of itself.
+def fun_rotatestrings(s, n):
+	l = len(s)
+	for i in range(n):
+		s = s[1:] + s[:1]
+	return s
+	
 
 def isrotation(x, y):
 	# Your code goes here
-	pass
+	if(x == y):
+		return True
+	t = str(x)
+	for i in range(len(t)):
+		r = fun_rotatestrings(t,i)
+		if(int(r) == y):
+			return True
+	return False
+
+print(isrotation(1234,1234))
