35d3aaa4769582b34140104573ebc74f9cd3e731
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-18 15:03:54 +0530

    shortenlongruns

--- 09-shortenlongruns-Python/shortenlongruns.py
+++ 09-shortenlongruns-Python/shortenlongruns.py
@@ -8,7 +8,39 @@
 # Note: your function may not just create a copy of L and call the destructive version of this function (below) on 
 # that copy and return it. Instead, you must directly construct the result here.
 
+def lookandsay(a):
+    print(type(a))
+    b = a
+    b.append(" ")
+    print(b)
+    result = []
+    repeat = b[0]
+    b = b[1:]
+    count = 1
+    if(len(a) == 0):
+        return result
+    for item in b:
+        if item != repeat:
+            result.append((count,int(repeat)))
+            count = 1
+            repeat = item
+        else:
+            count += 1
+    return result
 
 def shortenlongruns(L, k):
+	m = lookandsay(L)
 	# Your code goes here
-	pass
+	l = []
+	if(len(L) == 0):
+		return l
+	for item in m:
+		if(len(item) == 0):
+			return l
+		for i in range(item[0]):
+			if(i == k - 1):
+				break
+			l.append(item[1])
+	return l
+
+print(shortenlongruns([2, 3, 5, 5, 5, 3], 3))
