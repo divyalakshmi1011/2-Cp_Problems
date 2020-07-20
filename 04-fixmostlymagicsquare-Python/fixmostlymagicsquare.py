26656c7a75ad803f7727f080eb454e9a13577921
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-20 11:25:48 +0530

    Jul 20  11:25:47

--- 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
+++ 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
@@ -4,8 +4,30 @@
 # exercise, we assume we have just such a list L, and your task is to find and fix that change. So, given the list 
 # L, return a new list M such that M is the same as L, only with exactly one value changed, and M is a mostly magic 
 # square.
+def ismostlymagicsquare(arr):
+    d1 = 0
+    d2 = 0
+    l = []
+    for i in range(0, len(arr)): 
+        sum = 0
+        for j in range(0, len(arr[0])): 
+            if (i == j): 
+                d1 += arr[i][j] 
+            if (i == len(arr[0]) - j - 1): 
+                d2 += arr[i][j]
+            sum += arr[i][j]
+        l.append(sum)
+    l.append(d1)
+    l.append(d2)
+    return l
 
 
 def fixmostlymagicsquare(L):
-	pass
-	# Your code goes here
+	k = ismostlymagicsquare(L)
+	if len(set(k)) == 1 :
+		return L
+	else:
+		x = list(set(k))
+		return x[1]
+
+print(fixmostlymagicsquare([[2, 7, 9], [9, 5, 1], [4, 3, 8]]))
