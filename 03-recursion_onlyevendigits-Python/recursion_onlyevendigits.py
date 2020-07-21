382e24ef5d1260f63e61be46103154a89a243f29
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-21 11:13:00 +0530

    onlyevenrecursion-python

--- 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
+++ 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
@@ -7,20 +7,13 @@
 # So: onlyEvenDigits([43, 23265, 17, 58344]) returns [4, 226, 0, 844]. 
 # Also the function returns the empty list if the original list is empty. 
 # Remember to not use strings. You may not use loops/iteration in this problem.
-l = []
-def iseven(n):
-	if(n == 0):
-		return l
-	rem = n % 10
-	k = n // 10
-	print(k,rem)
-	if(rem % 2) == 0:
-		print("divya",rem)
-		l.append(rem)
-		print(l)
-	else:
-		iseven(k)
 
-print(iseven(143))
+def evenlis(lis):  
+    if len(lis) == 0:
+        return []  
+    else:
+        return [lis[0]] if lis[0] % 2 == 0 else [] + evenlis(lis[1:])
+
+print(evenlis([143]))
 def fun_recursion_onlyevendigits(l): 
 		return []
