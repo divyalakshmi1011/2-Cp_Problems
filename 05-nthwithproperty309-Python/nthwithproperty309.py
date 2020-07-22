c678e28d9b78e941e94630fbfa20f77799e994f5
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-22 12:35:56 +0530

    Jul 22  12:35:56

--- 05-nthwithproperty309-Python/nthwithproperty309.py
+++ 05-nthwithproperty309-Python/nthwithproperty309.py
@@ -6,4 +6,24 @@
 
 def nthwithproperty309(n):
 	# Your code goes here
-	pass
+	if(n == 0):
+		return 309
+	x = n**5
+	s = str(x)
+	i = 310
+	count = 0
+	while(count <= n):
+		print(i)
+		x = i**5
+		s = str(x)
+		if('0' in s and '1' in s and '2' in s and '3' in s and '4' in s and '5' in s and '6' in s and '7' in s and '8' in s and '9' in s):
+			count = count + 1
+			if(count == n):
+				break
+			else:
+				i = i + 1
+		else:
+			i = i + 1
+	return i
+
+print(nthwithproperty309(3))
