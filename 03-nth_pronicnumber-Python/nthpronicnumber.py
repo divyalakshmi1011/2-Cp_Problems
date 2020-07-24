ab25de392ab5e77e7c63993c8c19467231ea4b2c
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-24 11:07:35 +0530

    proconic-python

--- 03-nth_pronicnumber-Python/nthpronicnumber.py
+++ 03-nth_pronicnumber-Python/nthpronicnumber.py
@@ -4,7 +4,7 @@
 # number n is a product of x and (x+1).
 def factors(n):
     l = []
-    for i in range(1,n + 1):
+    for i in range(2,n + 1):
         if(n % i == 0):
                 l.append(i)
     return l
@@ -12,7 +12,8 @@ def ispronic(n):
 	l = factors(n)
 	for i in range(len(l) - 1):
 		# print(l[i],l[i + 1])
-		if(l[i] == l[i + 1] - 1):
+		if(l[i] == l[i + 1] - 1 and (l[i] * l[i + 1] == n)):
+			print(l[i],l[i + 1])
 			return True
 	return False
 
@@ -20,8 +21,10 @@ def nthpronicnumber(n):
 	# Your code goes here
 	if(n == 0):
 		return 0
+	if(n == 1):
+		return 2
 	c = 2
-	count = 0
+	count = 1
 	while(count <= n):
 		# print("c",c)
 		if(ispronic(c)):
