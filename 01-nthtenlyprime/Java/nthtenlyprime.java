ba0e4297fcbe61f6b5d6d1af82841f8515de4136
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-25 15:21:16 +0530

    Jul 25  15:21:16

--- 01-nthtenlyprime/Java/nthtenlyprime.java
+++ 01-nthtenlyprime/Java/nthtenlyprime.java
@@ -5,7 +5,50 @@
 
 
 class nthtenlyprime {
-	public int fun_nthtenlyprime(int n){
-		return 1;
+	public static boolean isprime(int n) {
+		int count = 0;
+		for(int i = 1; i <= n; i++) {
+			if(n % i == 0) {
+				count += 1;
+			}
+		}
+		if(count == 2) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public static int fun_nthtenlyprime(int n){
+		// String a = Integer.toString(n);
+		int c = 0;
+		int t = 2;
+		while(c <= n) {
+		String a = Integer.toString(t);
+		if(a.length() == 1) {
+			t += 1;
+		} else {
+			int sum = 0;
+			for(int i = 0; i < a.length(); i++) {
+				int b = Character.getNumericValue(a.charAt(i));
+				sum += b;
+			}
+			System.out.println(t + " " + sum);
+			if(isprime(sum)) {
+				c += 1;
+				if(c == n) {
+					break;
+				} else {
+					t += 1;
+				}
+			} else {
+				t += 1;
+			}
+		}
+	}
+	return t;
+	}
+	public static void main(String[] args) {
+		System.out.println(isprime(50));
+		System.out.println(fun_nthtenlyprime(3));
 	}
 }
