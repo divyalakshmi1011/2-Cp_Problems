2e5647ce5b6595eb62becc2f4c934857a539d226
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-25 11:38:55 +0530

    Jul 25  11:38:55

--- 06-leastfrequentletters-Python/leastfrequentletters.py
+++ 06-leastfrequentletters-Python/leastfrequentletters.py
@@ -8,5 +8,17 @@
 # if s does not contain any alphabetic characters, the result should be the empty string ("")
 
 def leastfrequentletters(s):
-	# Your code goes here
-	pass
+	s = s.replace(" ","")
+	s = s.lower()
+	d = {}
+	for i in s:
+		if i.isalpha():
+			if i not in d:
+				d[i] = 1
+			else:
+				d[i] += 1
+	l = list(d.values())
+	m = min(l)
+	print(m)
+
+leastfrequentletters("aDq efQ? FB'daf!!!")
