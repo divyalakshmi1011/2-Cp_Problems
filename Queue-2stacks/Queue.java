ffaa72482a4986f8b0eba16c719b84e5feb78b59
divyalakshmi1011 <55688130+divyalakshmi1011@users.noreply.github.com>
2020-07-29 15:34:56 +0530

    Jul 29  15:34:55

---
+++ Queue-2stacks/Queue.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Queue{
+    static Stack<Integer> s1 = new Stack<Integer>();
+    static Stack<Integer> s2 = new Stack<Integer>();
+    Queue() {
+
+    }
+    public static void push(int a) {
+        s1.push(a);
+    }
+    public static int pop() {
+        Iterator<Integer> it = s1.iterator();
+        while(it.hasNext()){
+            int temp = it.next();
+            s2.push(temp);
+        }
+        int p = s2.pop();
+        return p;
+    }
+    public static void main(String[] args) {
+        push(1);
+        push(2);
+        push(3);
+    }
+}
